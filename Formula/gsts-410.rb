e0ddbc12642689a4dc29e9e7f7addf1ef0b39555
Jarrett Irons <jarrett@whoco.com>
2023-03-30 12:57:45 -0700

    added seperate 4.1.0 version

---
+++ Formula/gsts-410.rb
@@ -0,0 +1,28 @@
+require "language/node"
+
+class Gsts410 < Formula
+  desc "Obtain and store AWS STS credentials by authenticating via G Suite SAML"
+  homepage "https://github.com/ruimarinho/gsts"
+  url "https://github.com/ruimarinho/gsts/archive/v4.1.0.tar.gz"
+  sha256 "7ef28327287816e3b5a25440b72b9cf425f9de2384c6f1397c419579d0f344cb"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    ENV["PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD"]="1"
+
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    (bin/"gsts").write_env_script libexec/"lib/node_modules/gsts/index.js", PLAYWRIGHT_BROWSERS_PATH: "0"
+  end
+
+  def post_install
+    ENV["PLAYWRIGHT_BROWSERS_PATH"]="0"
+
+    system "#{Formula["node"].bin}/node", "#{libexec}/lib/node_modules/gsts/node_modules/playwright/install.js"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gsts --version")
+  end
+end
